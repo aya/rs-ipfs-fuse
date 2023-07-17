57186d1c7de3570caf046c9e1c0598e10fe0d392
aynic.os <support+git@asycn.io>
2023-07-17 23:59:54 +0000

    init fuse-ipfs project

---
+++ fuse-ipfs/src/main.rs
@@ -0,0 +1,23 @@
+extern crate fuser;
+
+use std::env;
+use fuser::Filesystem;
+
+struct IpfsFilesystem;
+
+impl Filesystem for IpfsFilesystem {
+}
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+  match args.len() {
+    // one argument passed
+    2 => {
+      let mountpoint = &args[1];
+      fuser::mount2(IpfsFilesystem, &mountpoint, &[]);
+    }
+    _ => {
+      println!("Usage: {} <mountpoint>", args[0]);
+    }
+  }
+}
